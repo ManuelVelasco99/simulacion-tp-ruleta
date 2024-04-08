bcddbf0c384a41c10a0f9d0cf7d9b42a4ab2a0c7
DeBer24 <142444363+DeBer24@users.noreply.github.com>
2024-04-08 17:08:05 -0300

    V3.0
    Agregué los cálculos de las medidas para todas las corridas, títulos y leyendas a los gráficos

--- main.py
+++ main.py
@@ -12,17 +12,18 @@ def generarGrafico(titulo, ejeX, ejeY, lista1, esperado):
     
     lista1.insert(0, 0)
     plt.xlim(1, numero_tiradas + 1)
-    plt.plot(lista1)
+    plt.plot(lista1, label = "Valor obtenido")
 
     puntosX = np.array([1, numero_tiradas])
     puntosY = np.array([esperado, esperado])
-    plt.plot(puntosX, puntosY)
+    plt.plot(puntosX, puntosY, label = "Valor esperado")
     
+    plt.legend()
     plt.plot()
     plt.show()
 
 
-# === Setear Argumentos
+#region === Setear Argumentos
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--corridas", help="Valor del numero de corridas (15 por defecto)", default=15)
 parser.add_argument("-t", "--tiradas", help="Valor del numero de tiradas por corrida (2000 por defecto)", default=2000)
@@ -32,44 +33,84 @@ numero_corridas = int(args.corridas)
 numero_tiradas = int(args.tiradas)
 numero = int(args.numero)
 print("Ejecutando " + str(numero_corridas) + " corridas de " + str(numero_tiradas) + " tiradas para el número " + str(numero))
-# ===
+#endregion
 
 
-# === Generar los valores aleatoreos
+#region === Generar los valores aleatorios
 corridas = []
 for i in range(numero_corridas):
     corridas.append([random.randint(0, 36) for _ in range(int(numero_tiradas))])
-# ===
+#endregion
 
-# === Seleccionar una corrida al azar, para mostrar gráficos individuales
+
+#region === Seleccionar una corrida al azar, para mostrar gráficos individuales
 num_corrida = random.randint(1, numero_corridas) - 1
 corrida_seleccionada = corridas[num_corrida]
-# ===
+#endregion
 
-#print(corrida_seleccionada)
+#print(corridas[0])
+#print(corridas[1])
+#print(corridas[2])
 
-# === Generar los valores relativos de la corrida seleccionada
+#region === Generar valores frecuencia relativa
+general_frecuencia = []
+apariciones = 0
+
+for i in range(numero_tiradas):
+    for c in corridas:
+        if c[i] == numero:
+            apariciones += 1
+    
+    general_frecuencia.append(apariciones / ((i + 1) * numero_corridas))
+
+
 corrida_seleccionada_frecuencia = []
+apariciones = 0
 
-numero_apariciones = 0
 for i in range(numero_tiradas):
     if corrida_seleccionada[i] == numero:
-        numero_apariciones += 1
-    corrida_seleccionada_frecuencia.append(numero_apariciones / (i + 1))
-# ===
+        apariciones += 1
+    corrida_seleccionada_frecuencia.append(apariciones / (i + 1))
+#endregion
 
 
-# === Generar valor promedio de la corrida seleccionada
-corrida_seleccionada_promedio = []
+#region === Generar valores promedio
+general_promedio = []
+corridas_promedios = []
 total = 0
 
 for i in range(numero_tiradas):
-    total += corrida_seleccionada[i]
-    corrida_seleccionada_promedio.append(total / (i + 1))
-# ===
+    for c in corridas:
+        total += c[i]
+    general_promedio.append(total / ((i + 1) * numero_corridas))
 
 
-# === Generar varianza de cada corrida
+for c in corridas:
+    aux = []
+    total = 0
+    for i in range(numero_tiradas):
+        total += c[i]
+        aux.append(total / (i + 1))
+    corridas_promedios.append(aux)
+
+
+corrida_seleccionada_promedio = corridas_promedios[num_corrida]
+#endregion
+
+
+#region === Generar valores varianza
+general_varianza = []
+
+for tirada in range(numero_tiradas):
+    total = 0
+    for c in corridas:
+        for i in range(tirada + 1):
+            total += (c[i] - corridas_promedios[corridas.index(c)][tirada]) ** 2
+            a = 1
+    
+    general_varianza.append(total / ((tirada + 1) * numero_corridas))
+
+
 corrida_seleccionada_varianza = []
 
 for tirada in range(numero_tiradas):
@@ -77,10 +118,16 @@ for tirada in range(numero_tiradas):
     for i in range(tirada + 1):
         total += (corrida_seleccionada[i] - corrida_seleccionada_promedio[tirada]) ** 2
     corrida_seleccionada_varianza.append(total / (tirada + 1))
-# ===
+#endregion
 
 
-# === Generar desvio estandar de cada corrida
+# === Generar valores desvio estandar
+general_desvio = []
+
+for tirada in range(numero_tiradas):
+    general_desvio.append(math.sqrt(general_varianza[tirada]))
+
+
 corrida_seleccionada_desvio = []
 
 for tirada in range(numero_tiradas):
@@ -88,20 +135,25 @@ for tirada in range(numero_tiradas):
 # ===
 
 
-
-# === Generando grafico de la frecuencia relativa respecto del numero de tiradas
+#region Gráficos
 valor_esperado_frecuencia = 1/37
-generarGrafico('Corrida número ' + str(num_corrida + 1), 'n (número de tiradas)', 'fr (frecuencia relativa)', corrida_seleccionada_frecuencia, valor_esperado_frecuencia)
+generarGrafico('Frecuencia relativa - Corrida número ' + str(num_corrida + 1), 'n (número de tiradas)', 'fr (frecuencia relativa)', corrida_seleccionada_frecuencia, valor_esperado_frecuencia)
 
-
-# === Generando grafico del valor promedio respecto del numero de tiradas
 valor_esperado_promedio = 18
-generarGrafico('Corrida número ' + str(num_corrida + 1), 'n (número de tiradas)', 'vp (valor promedio de las tiradas)', corrida_seleccionada_promedio, valor_esperado_promedio)
+generarGrafico('Valor promedio - Corrida número ' + str(num_corrida + 1), 'n (número de tiradas)', 'vp (valor promedio de las tiradas)', corrida_seleccionada_promedio, valor_esperado_promedio)
 
+valor_esperado_varianza = ((36 - 0 + 1) ** 2 - 1) / 12
+generarGrafico('Varianza - Corrida número ' + str(num_corrida + 1), 'n (número de tiradas)', 'vv (valor de la varianza)', corrida_seleccionada_varianza, valor_esperado_varianza)
 
-# === Generando grafico de la varianza con respecto del numero de tiradas
-generarGrafico('Corrida número ' + str(num_corrida + 1), 'n (número de tiradas)', 'vv (valor de la varianza)', corrida_seleccionada_varianza, 1)
+valor_esperado_desvio = math.sqrt(((36 - 0 + 1) ** 2 - 1) / 12)
+generarGrafico('Desvío estándar - Corrida número ' + str(num_corrida + 1), 'n (número de tiradas)', 'vd (valor del desvío)', corrida_seleccionada_desvio, valor_esperado_desvio)
 
+generarGrafico('Frecuencia relativa - Promedio de todas las corridas', 'n (número de tiradas)', 'fr (frecuencia relativa)', general_frecuencia, valor_esperado_frecuencia)
 
-# === Generando grafico del desvio estandar con respecto del numero de tiradas
-generarGrafico('Corrida número ' + str(num_corrida + 1), 'n (número de tiradas)', 'vd (valor del desvío)', corrida_seleccionada_desvio, 1)
+generarGrafico('Valor promedio - Promedio de todas las corridas', 'n (número de tiradas)', 'vp (valor promedio de las tiradas)', general_promedio, valor_esperado_promedio)
+
+generarGrafico('Varianza - Promedio de todas las corridas', 'n (número de tiradas)', 'vv (valor de la varianza)', general_varianza, valor_esperado_varianza)
+
+generarGrafico('Desvío estándar - Promedio de todas las corridas', 'n (número de tiradas)', 'vd (valor del desvío)', general_desvio, valor_esperado_desvio)
+
+#endregion
