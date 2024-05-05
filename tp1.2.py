0f281a9272eed0e070188b331c0ec6a2a048b4f4
DeBer24 <142444363+DeBer24@users.noreply.github.com>
2024-05-05 17:17:39 -0300

    tp1.2 - V2.0
    Estrategias D'Alembert y Fibonacci, gráfico de frecuencia y se agregaron todas las selecciones posibles para jugar

--- tp1.2.py
+++ tp1.2.py
@@ -2,14 +2,11 @@ import random
 import matplotlib.pyplot as plt
 import argparse
 import numpy as np
-
+#random.seed(11554547)
 
 class Ruleta:
     rojo = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
     negro = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
-    primeraColumna = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
-    segundaColumna = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
-    terceraColumna = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
     
     def __init__(self, min, max):
         self.apuestaMinima = min
@@ -20,15 +17,34 @@ class Ruleta:
 
         if isinstance(seleccion, int):
             return seleccion == numero
-        elif seleccion == 'rojo':
-            return numero in self.rojo
-        elif seleccion == 'negro':
-            return numero in self.negro
-        #seguir con las opciones. Para par o impar y las docenas no hace falta hacer lista, se puede calcular
+        else:
+            match seleccion:
+                case 'rojo':
+                    return numero in self.rojo
+                case 'negro':
+                    return numero in self.negro
+                case 'par':
+                    return numero % 2 == 0 and numero != 0
+                case 'impar':
+                    return numero % 2 != 0
+                case 'doc1':
+                    return numero > 0 and numero < 13
+                case 'doc2':
+                    return numero > 12 and numero < 25
+                case 'doc3':
+                    return numero > 24 and numero < 37
+                case 'col1':
+                    return numero % 3 == 1
+                case 'col2':
+                    return numero % 3 == 2
+                case 'col3':
+                    return numero % 3 == 0 and numero != 0
 
 
 class Apuesta:
     capital = []
+    ganadas = [0]
+
     def __init__(self, seleccion, multiplicador, estrategia, capital, apuestaMinima):
         self.seleccion = seleccion
         self.multiplicador = multiplicador
@@ -47,13 +63,18 @@ class Apuesta:
 
     def calcularCapital(self, gana):
         nuevoCapital = self.capital[-1] - self.apuestaActual
+        g = self.ganadas[-1] * (len(self.ganadas) - 1)
         if gana:
             nuevoCapital = nuevoCapital + self.apuestaActual * self.multiplicador
+            g += 1
         
         self.capital.append(nuevoCapital)
+        self.ganadas.append(g / len(self.ganadas))
     
     def calcularSigApuesta(self, gana, min, max):
         aux = self.estrategia.calcularSigApuesta(gana, self.apuestaActual, min)
+        if aux < min:
+            aux = min
         if max:
             if (aux > max):
                 aux = max
@@ -76,39 +97,75 @@ class Martingala:
             return apuestaActual * 2
 
 class Dalembert:
-    pass
+    def calcularSigApuesta(gana, apuestaActual, apuestaMinima):
+        if gana:
+            return apuestaActual - apuestaMinima
+        else:
+            return apuestaActual + apuestaMinima
 
 class Fibonacci:
-    pass
+    nAnterior = 1
+    
+    def calcularSigApuesta(gana, apuestaActual, apuestaMinima):
+        if gana:
+            Fibonacci.nAnterior -= 2
+            if Fibonacci.nAnterior < 1: Fibonacci.nAnterior = 1
+        else:
+            Fibonacci.nAnterior += 1
+            
+        return Fibonacci.calcularTerminoNFib() * apuestaMinima
+
+    def calcularTerminoNFib(): #0 1 1 2 3 5 8 13 21
+        if Fibonacci.nAnterior == 1: return 1
+        if Fibonacci.nAnterior == 2: return 1
+        
+        aux1 = 1
+        aux2 = 1
+        for _ in range(3, Fibonacci.nAnterior + 1):
+            aux3 = aux1 + aux2
+            aux1 = aux2
+            aux2 = aux3
+        return aux3
 
 class Otra:
     pass
 
 
-
-def generarGrafico(titulo, nro_subplot, ejeX, ejeY, lista1, esperado):
+def generarGraficoLineas(titulo, nro_subplot, ejeX, ejeY, lista, esperado):
     plt.subplot(1, 2, nro_subplot)
     plt.title(titulo)
     plt.xlabel(ejeX)
     plt.ylabel(ejeY)
-
     
-    lista1.insert(0, 0)
-    plt.xlim(1, len(lista1) + 1)
-    plt.plot(lista1, label = "Capital tirada n")
-
-    puntosX = np.array([1, len(lista1)])
+    plt.plot(lista, label = "Capital tirada n")
+    
+    puntosX = np.array([0, len(lista) - 1])
     puntosY = np.array([esperado, esperado])
     plt.plot(puntosX, puntosY, label = "Capital inicial")
     
     plt.legend()
     plt.plot()
 
+def generarGraficoBarras(titulo, nro_subplot, ejeX, ejeY, lista, esperado):
+    plt.subplot(1, 2, nro_subplot)
+    plt.title(titulo)
+    plt.xlabel(ejeX)
+    plt.ylabel(ejeY)
+    plt.ylim(0, 1)
+    plt.bar(x = np.arange(len(lista)) + 1, height = lista)
+
+    puntosX = np.array([0, len(lista) + 1])
+    puntosY = np.array([esperado, esperado])
+    plt.plot(puntosX, puntosY, label = "Valor esperado", color = "darkorange")
+    plt.legend()
+    plt.plot()
+
+
 
 #region === Setear Argumentos
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--corridas", help="Valor del numero de corridas (15 por defecto)", default=15)
-parser.add_argument("-n", "--seleccion", help="Selección: un número, \"rojo\", \"negro\", ... - rojo por defecto", default='rojo')
+parser.add_argument("-n", "--seleccion", help="Selección: un número, \"rojo\", \"negro\", \"par\", \"impar\", \"docena1\", \"docena2\", \"docena3\", \"col1\", \"col2\", \"col3\" - rojo por defecto", default='rojo')
 parser.add_argument("-s", "--estrategia", help="Estrategia a utilizar: \"m\" - Martingala, \"d\" - D'Alemnert, \"f\" - Fibonacci, \"o\" - Definir otra - m por defecto", default='m')
 parser.add_argument("-a", "--capital", help="Capital: \"i\" para infinito o el monto si es finito - 30000 por defecto", default=30000)
 args, unknown = parser.parse_known_args()
@@ -117,10 +174,12 @@ numCorridas = int(args.corridas)
 seleccion = args.seleccion
 estrategia = args.estrategia
 
-if isinstance(seleccion, int):
+if seleccion.isdigit():
     seleccion = int(seleccion)
 
-if isinstance(args.capital, int): 
+if isinstance(args.capital, int):
+    capitalInicial = args.capital
+elif args.capital.isdigit(): 
     capitalInicial = int(args.capital)
 else:
     capitalInicial = False
@@ -130,7 +189,19 @@ print("Ejecutando ruleta.\nNúmero de corridas: " + str(numCorridas) + " - Selec
 
 
 
-multiplicador = 2 #crear método para asignarlo dinámicamente
+if (isinstance(seleccion, int)):
+    multiplicador = 36
+    frEsperada = 1/37
+elif (seleccion == 'rojo' or seleccion == 'negro' or seleccion == 'par' or seleccion == 'impar'):
+    multiplicador = 2
+    frEsperada = 18/37
+elif (seleccion == 'doc1' or seleccion == 'doc2' or seleccion == 'doc3' or seleccion == 'col1' or seleccion == 'col2' or seleccion == 'col3'):
+    multiplicador = 3
+    frEsperada = 12/37
+else:
+    print('La selección no es válida')
+    exit()
+
 
 apuestaMinima = 1000
 apuestaMaxima = False
@@ -157,10 +228,11 @@ while apuesta.apuestaActual != -1 and apuesta.capital[-1] < capitalObjetivo:
 
 
 
-generarGrafico('Capital', 1, 'n (número de tirada)', 'cc (cantidad de capital)', apuesta.capital, capitalInicial)
+generarGraficoLineas('Capital', 1, 'n (número de tirada)', 'cc (cantidad de capital)', apuesta.capital, capitalInicial)
+apuesta.ganadas.pop(0)
+generarGraficoBarras('Frecuencia relativa apuesta favorable', 2, 'n (número de tiradas)', 'fr (frecuencia relativa)', apuesta.ganadas, frEsperada)
 
 #plt.suptitle('CORRIDA NÚMERO ' + str(num_corrida + 1))
-
 figManager = plt.get_current_fig_manager()
 figManager.resize(1000, 500)
 plt.tight_layout()
