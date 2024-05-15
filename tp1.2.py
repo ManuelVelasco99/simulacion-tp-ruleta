9a0fb6f837eee5d305849963a110a17771122018
DeBer24 <142444363+DeBer24@users.noreply.github.com>
2024-05-14 23:31:23 -0300

    tp1.2 - V3.0
    Se agregaron múltiples corridas con sus respectivos gráficos.
    Se creó la 4ta estrategia.

--- tp1.2.py
+++ tp1.2.py
@@ -2,7 +2,6 @@ import random
 import matplotlib.pyplot as plt
 import argparse
 import numpy as np
-#random.seed(11554547)
 
 class Ruleta:
     rojo = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
@@ -42,14 +41,14 @@ class Ruleta:
 
 
 class Apuesta:
-    capital = []
-    ganadas = [0]
-
     def __init__(self, seleccion, multiplicador, estrategia, capital, apuestaMinima):
+        self.capital = []
+        self.ganadas = [0]
         self.seleccion = seleccion
         self.multiplicador = multiplicador
         self.capital.append(capital)
         self.apuestaActual = apuestaMinima
+        
 
         match (estrategia):
             case 'm':
@@ -58,8 +57,8 @@ class Apuesta:
                 self.estrategia = Dalembert
             case 'f':
                 self.estrategia = Fibonacci
-            case 'o':
-                self.estrategia = Otra
+            case 'p':
+                self.estrategia = Paroli
 
     def calcularCapital(self, gana):
         nuevoCapital = self.capital[-1] - self.apuestaActual
@@ -127,113 +126,154 @@ class Fibonacci:
             aux2 = aux3
         return aux3
 
-class Otra:
-    pass
+class Paroli:
+    contador = 0
 
+    def calcularSigApuesta(gana, apuestaActual, apuestaMinima):
+        if gana:
+            Paroli.contador += 1
+        if (not gana) or Paroli.contador == 3 :
+            Paroli.contador = 0
+            return apuestaMinima
+        else:
+            return apuestaActual * 2
+        
 
-def generarGraficoLineas(titulo, nro_subplot, ejeX, ejeY, lista, esperado):
+def generarGraficoLineas(titulo, nro_subplot, ejeX, ejeY, lista, esperado, multiplesListas = False):
     plt.subplot(1, 2, nro_subplot)
     plt.title(titulo)
     plt.xlabel(ejeX)
     plt.ylabel(ejeY)
-    
-    plt.plot(lista, label = "Capital tirada n")
-    
-    puntosX = np.array([0, len(lista) - 1])
+    aux = 0
+
+    if multiplesListas:
+        for x in range(numCorridas):
+            plt.plot(lista[x].capital)
+            if len(lista[x].capital) > aux: aux = len(lista[x].capital)
+    else:
+        plt.plot(lista, label = "Capital tirada n")
+        aux = len(lista)
+
+    puntosX = np.array([0, aux - 1])
     puntosY = np.array([esperado, esperado])
     plt.plot(puntosX, puntosY, label = "Capital inicial")
     
     plt.legend()
     plt.plot()
 
-def generarGraficoBarras(titulo, nro_subplot, ejeX, ejeY, lista, esperado):
+def generarGraficoBarras(titulo, nro_subplot, ejeX, ejeY, lista, esperado, multiplesListas = False):
     plt.subplot(1, 2, nro_subplot)
     plt.title(titulo)
     plt.xlabel(ejeX)
     plt.ylabel(ejeY)
     plt.ylim(0, 1)
-    plt.bar(x = np.arange(len(lista)) + 1, height = lista)
 
-    puntosX = np.array([0, len(lista) + 1])
-    puntosY = np.array([esperado, esperado])
-    plt.plot(puntosX, puntosY, label = "Valor esperado", color = "darkorange")
-    plt.legend()
+    if multiplesListas:
+        for x in range(numCorridas):
+            lista[x].ganadas.pop(0)
+            plt.bar(x = np.arange(len(lista[x].ganadas)) + 1, height = lista[x].ganadas)
+    else:
+        lista.pop(0)
+        plt.bar(x = np.arange(len(lista)) + 1, height = lista)
+        puntosX = np.array([0, len(lista) + 1])
+        puntosY = np.array([esperado, esperado])
+        plt.plot(puntosX, puntosY, label = "Valor esperado", color = "darkorange")
+        plt.legend()
+
     plt.plot()
 
+def mostrarGrafico(titulo):
+    plt.suptitle(titulo)
+    figManager = plt.get_current_fig_manager()
+    figManager.resize(1000, 500)
+    plt.tight_layout()
+    plt.show()
+
+def tipoSeleccion(arg):
+        opciones = ['rojo', 'negro', 'par', 'impar', 'docena1', 'docena2', 'docena3', 'col1', 'col2', 'col3']
+        if str.lower(arg) in opciones:
+            return str.lower(arg)
+        elif arg.isdigit() and int(arg) >= 0 and int(arg) <= 36:
+            return int(arg)
+        raise argparse.ArgumentTypeError('La selección ingresada no es válida')
+
+def tipoCapital(arg):
+    if str.lower(arg) == 'i':
+        return str.lower(arg)
+    elif arg.isdigit() and int(arg) >= 1000:
+        return int(arg)
+    raise argparse.ArgumentTypeError('El capital ingresado no es válido')
+
+def main():
+    if (isinstance(seleccion, int)):
+        multiplicador = 36
+        frEsperada = 1/37
+    elif (seleccion == 'rojo' or seleccion == 'negro' or seleccion == 'par' or seleccion == 'impar'):
+        multiplicador = 2
+        frEsperada = 18/37
+    elif (seleccion == 'doc1' or seleccion == 'doc2' or seleccion == 'doc3' or seleccion == 'col1' or seleccion == 'col2' or seleccion == 'col3'):
+        multiplicador = 3
+        frEsperada = 12/37
 
 
-#region === Setear Argumentos
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--corridas", help="Valor del numero de corridas (15 por defecto)", default=15)
-parser.add_argument("-n", "--seleccion", help="Selección: un número, \"rojo\", \"negro\", \"par\", \"impar\", \"docena1\", \"docena2\", \"docena3\", \"col1\", \"col2\", \"col3\" - rojo por defecto", default='rojo')
-parser.add_argument("-s", "--estrategia", help="Estrategia a utilizar: \"m\" - Martingala, \"d\" - D'Alemnert, \"f\" - Fibonacci, \"o\" - Definir otra - m por defecto", default='m')
-parser.add_argument("-a", "--capital", help="Capital: \"i\" para infinito o el monto si es finito - 30000 por defecto", default=30000)
-args, unknown = parser.parse_known_args()
+    apuestaMinima = 1000
+    apuestaMaxima = False
+    capitalObjetivo = 100000
 
-numCorridas = int(args.corridas)
-seleccion = args.seleccion
-estrategia = args.estrategia
+    if (capitalInicial):
+        apuestaMaxima = 20000
+        capitalObjetivo = capitalInicial * 10
 
-if seleccion.isdigit():
-    seleccion = int(seleccion)
+    ruleta = Ruleta(apuestaMinima, apuestaMaxima)
+    apuestas = []
 
-if isinstance(args.capital, int):
-    capitalInicial = args.capital
-elif args.capital.isdigit(): 
-    capitalInicial = int(args.capital)
-else:
-    capitalInicial = False
+    for x in range(numCorridas):
+        apuesta = Apuesta(seleccion, multiplicador, estrategia, capitalInicial, apuestaMinima)
+        print('\nCorrida ' + str(x))
+        i = 1
 
-print("Ejecutando ruleta.\nNúmero de corridas: " + str(numCorridas) + " - Selección: " + str(seleccion) + " - Estrategia: " + estrategia + " - Capital: " + str(capitalInicial))
-#endregion
-
-
-
-if (isinstance(seleccion, int)):
-    multiplicador = 36
-    frEsperada = 1/37
-elif (seleccion == 'rojo' or seleccion == 'negro' or seleccion == 'par' or seleccion == 'impar'):
-    multiplicador = 2
-    frEsperada = 18/37
-elif (seleccion == 'doc1' or seleccion == 'doc2' or seleccion == 'doc3' or seleccion == 'col1' or seleccion == 'col2' or seleccion == 'col3'):
-    multiplicador = 3
-    frEsperada = 12/37
-else:
-    print('La selección no es válida')
-    exit()
-
-
-apuestaMinima = 1000
-apuestaMaxima = False
-capitalObjetivo = 100000
-
-if (capitalInicial):
-    apuestaMaxima = 20000
-    capitalObjetivo = capitalInicial * 10
-
-
-ruleta = Ruleta(apuestaMinima, apuestaMaxima)
-apuesta = Apuesta(seleccion, multiplicador, estrategia, capitalInicial, apuestaMinima)
-
-i = 1
-
-while apuesta.apuestaActual != -1 and apuesta.capital[-1] < capitalObjetivo:
-    apuestaAnterior = apuesta.apuestaActual
-    gana = ruleta.girar(apuesta.seleccion)
-    apuesta.calcularCapital(gana)
-    apuesta.calcularSigApuesta(gana, ruleta.apuestaMinima, ruleta.apuestaMaxima)
-    print('Tirada ' + str(i) + ' - Apostó: ' + str(apuestaAnterior) + ' - Ganó: ' + str(gana) + ' - Nuevo capital: ' + str(apuesta.capital[-1]))
-    i+=1
+        while apuesta.apuestaActual != -1 and apuesta.capital[-1] < capitalObjetivo:
+            apuestaAnterior = apuesta.apuestaActual
+            gana = ruleta.girar(apuesta.seleccion)
+            apuesta.calcularCapital(gana)
+            apuesta.calcularSigApuesta(gana, ruleta.apuestaMinima, ruleta.apuestaMaxima)
+            print('Tirada ' + str(i) + ' - Apostó: ' + str(apuestaAnterior) + ' - Ganó: ' + str(gana) + ' - Nuevo capital: ' + str(apuesta.capital[-1]))
+            i+=1
+        
+        apuestas.append(apuesta)
 
 
 
+    numSeleccionada = random.randint(0, numCorridas - 1)
 
-generarGraficoLineas('Capital', 1, 'n (número de tirada)', 'cc (cantidad de capital)', apuesta.capital, capitalInicial)
-apuesta.ganadas.pop(0)
-generarGraficoBarras('Frecuencia relativa apuesta favorable', 2, 'n (número de tiradas)', 'fr (frecuencia relativa)', apuesta.ganadas, frEsperada)
+    generarGraficoLineas('Capital', 1, 'n (número de tirada)', 'cc (cantidad de capital)', apuestas[numSeleccionada].capital, capitalInicial)
+    generarGraficoBarras('Frecuencia relativa apuesta favorable', 2, 'n (número de tiradas)', 'fr (frecuencia relativa)', apuestas[numSeleccionada].ganadas, frEsperada)
+    mostrarGrafico('CORRIDA NÚMERO ' + str(numSeleccionada + 1))
 
-#plt.suptitle('CORRIDA NÚMERO ' + str(num_corrida + 1))
-figManager = plt.get_current_fig_manager()
-figManager.resize(1000, 500)
-plt.tight_layout()
-plt.show()
+    generarGraficoLineas('Capital', 1, 'n (número de tirada)', 'cc (cantidad de capital)', apuestas, capitalInicial, True)
+    generarGraficoBarras('Frecuencia relativa apuesta favorable', 2, 'n (número de tiradas)', 'fr (frecuencia relativa)', apuestas, frEsperada, True)
+    mostrarGrafico('MÚLTIPLES CORRIDAS')
+
+
+    #    print("Ejecutando ruleta.\nNúmero de corridas: " + str(numCorridas) + " - Selección: " + str(seleccion) + " - Estrategia: " + estrategia + " - Capital: " + str(capitalInicial))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--corridas', default=15, type=int, help='Valor del número de corridas (Por defecto: %(default)s)')
+    parser.add_argument('-n', '--seleccion', default='rojo', type=tipoSeleccion, help="Selección: un número, \"rojo\", \"negro\", \"par\", \"impar\", \"docena1\", \"docena2\", \"docena3\", \"col1\", \"col2\", \"col3\" (Por defecto: %(default)s)")
+    parser.add_argument('-s', '--estrategia', default='m', choices=['m','d','f','p'], help="Estrategia a utilizar: \"m\" - Martingala, \"d\" - D'Alemnert, \"f\" - Fibonacci, \"p\" - Paroli (Por defecto: %(default)s)", )
+    parser.add_argument('-a', '--capital', default=30000, type=tipoCapital, help="Capital: \"i\" para infinito o el monto si es finito (Por defecto: %(default)s - Mínimo 1000)")
+    args, unknown = parser.parse_known_args()
+
+    numCorridas = args.corridas
+    seleccion = args.seleccion
+    estrategia = args.estrategia
+
+    if args.capital == 'i':
+        capitalInicial = False
+    else:
+        capitalInicial = args.capital
+
+    print("Ejecutando ruleta.\nNúmero de corridas: " + str(numCorridas) + " - Selección: " + str(seleccion) + " - Estrategia: " + estrategia + " - Capital: " + str(capitalInicial))
+    main()
