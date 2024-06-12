1df125a18fd7a8f2c4f28aff467b7ecdcf16edf9
DeBer24 <142444363+DeBer24@users.noreply.github.com>
2024-06-12 16:15:13 -0300

    feat: test distribución uniforme

--- tp2.2/distUniforme.py
+++ tp2.2/distUniforme.py
@@ -1,6 +1,6 @@
 import random, numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import uniform
+from scipy.stats import uniform, chi2
 
 def f(a, b):
     return 1 / (b - a)
@@ -37,6 +37,46 @@ def graficar(valores, a, b, titulo):
     plt.legend()
     plt.show()
 
+def armarIntervalos(lista, a, b):
+    cantIntervalos = int(len(lista) / 10)
+    long = (b - a) / cantIntervalos #los numeros pertenecen al intervalo [a, b]
+    inicio = a
+    fin = inicio + long
+    intervalos = []
+
+    for _ in range(cantIntervalos):
+        contador = 0
+        for x in range(len(lista)):
+            if (lista[x] >= inicio and lista[x] < fin):
+                contador += 1
+
+        intervalos.append(contador)
+        inicio = fin
+        fin = fin + long
+    
+    intervalos[-1] += lista.count(b)
+    return intervalos
+
+def testChiCuadrado(lista, nc, a, b, titulo):
+    # para distribución uniforme
+    cantNumeros = len(lista)
+    intervalos = armarIntervalos(lista, a, b)
+    cantIntervalos = len(intervalos)
+    frecEsperada = cantNumeros / cantIntervalos
+
+    chiCuadrado = 0
+    for x in range(cantIntervalos):
+        aux = (intervalos[x] - frecEsperada) ** 2 / frecEsperada
+        chiCuadrado += aux
+
+    valorTabla = chi2.ppf(nc, cantIntervalos - 1)
+
+    print('\n' + titulo)
+    print('Prueba de bondad de ajuste chi cuadrado para distribución uniforme')
+    print(f"Estadístico obtenido: {chiCuadrado}")
+    print(f"Valor crítico: {valorTabla} - Nivel de confianza: {nc * 100}%")
+    print('Resultado: El generador pasa la prueba') if chiCuadrado < valorTabla else print('Resultado: El generador NO pasa la prueba')
+
 
 if __name__ == "__main__":
     a = 25
@@ -45,6 +85,9 @@ if __name__ == "__main__":
 
     transInversa = [randomTransInversa(a, b) for _ in range(cantValores)]
     rechazo = [randomRechazo(a, b) for _ in range(cantValores)]
+
+    testChiCuadrado(transInversa, 0.95, a, b, 'Método Transformada Inversa')
+    testChiCuadrado(rechazo, 0.95, a, b, 'Método de Rechazo')
 
     graficar(transInversa, a, b, 'Método Transformada Inversa')
     graficar(rechazo, a, b, 'Método de Rechazo')
