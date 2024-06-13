3bc45b2d49e255870a25b2542aced02a6e534105
DeBer24 <142444363+DeBer24@users.noreply.github.com>
2024-06-13 19:42:54 -0300

    feat: test para empírica

--- tp2.2/distEmpiricaDisc.py
+++ tp2.2/distEmpiricaDisc.py
@@ -1,6 +1,7 @@
+from cgi import test
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import ecdf
+from scipy.stats import ecdf, chi2
 
 def f(x, muestra, minimo, maximo):
     rango = maximo - minimo
@@ -49,12 +50,64 @@ def graficar(valores, muestra, minMuestra):
     plt.legend()
     plt.show()
 
+def testChiCuadrado(muestra1, muestra2, nc):
+    # hip nula: las dos muestras siguen la misma distribución
+    total = len(muestra1) + len(muestra2)
+    frecObservadasTotal = {}
+    
+    frecObservadas1 = {}
+    for m in muestra1:
+        if m in frecObservadas1:
+            frecObservadas1[m] += 1
+        else:
+            frecObservadas1[m] = 1
+        if m in frecObservadasTotal:
+            frecObservadasTotal[m] += 1
+        else:
+            frecObservadasTotal[m] = 1
+    
+    frecObservadas2 = {}
+    for m in muestra2:
+        if m in frecObservadas2:
+            frecObservadas2[m] += 1
+        else:
+            frecObservadas2[m] = 1
+        if m in frecObservadasTotal:
+            frecObservadasTotal[m] += 1
+        else:
+            frecObservadasTotal[m] = 1
+
+    frecEsperada1 = {}
+    frecEsperada2 = {}
+    for t in frecObservadasTotal:
+        aux = frecObservadasTotal[t] / total * len(muestra1)
+        frecEsperada1[t] = aux
+    for t in frecObservadasTotal:
+        aux = frecObservadasTotal[t] / total * len(muestra2)
+        frecEsperada2[t] = aux
+
+    chiM1, chiM2 = 0, 0
+    for t in frecObservadasTotal:
+        chiM1 += (frecObservadas1[t] - frecEsperada1[t])**2 / frecEsperada1[t]
+        chiM2 += (frecObservadas2[t] - frecEsperada2[t])**2 / frecEsperada2[t]
+    
+    chiTotal = chiM1 + chiM2
+    valorTabla = chi2.ppf(nc, len(frecObservadasTotal))
+
+    print()
+    print('\nPrueba de bondad de ajuste chi cuadrado')
+    print(f"Estadístico obtenido: {chiTotal}")
+    print(f"Valor crítico: {valorTabla} - Nivel de confianza: {nc * 100}%")
+    print('Resultado: El generador pasa la prueba') if chiTotal < valorTabla else print('Resultado: El generador NO pasa la prueba')
+
 
 if __name__ == "__main__":
     muestra = [12, 5, 4, 11, 9, 4, 9, 7, 4, 6, 14, 3]
     cantValores = 1000
 
     valoresGenerados = [randomRechazo(muestra) for _ in range(cantValores)]
+    testChiCuadrado(valoresGenerados, muestra, 0.95)
+
     frecAbsolutas = calcFrecAbs(muestra, valoresGenerados)
     graficar(frecAbsolutas, [], min(muestra))
     
